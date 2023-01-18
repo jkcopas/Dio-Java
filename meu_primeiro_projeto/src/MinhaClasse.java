17cbe5d4bc757f465a978ba48c959422ca92e2b2
jkcopas <ferreirapatrick.0@gmail.com>
2023-01-18 13:44:52 -0300

    Auala de clasees

---
+++ meu_primeiro_projeto/src/MinhaClasse.java
@@ -0,0 +1,23 @@
+public class MinhaClasse {
+    //Método main (principal)
+    public static void main (String [] args) {
+        //declaração de variaveis
+        String meuNome = "Patrick";
+        int anoNascimento = 1996;
+        boolean verdadeiro = true; 
+        
+        String primeiroNome = "Patrick";
+        String segundoNome = "Ferreira";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+//declaração de metodos
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        //return primeiroNome.concat(" ").concat(segundoNome);
+
+        return "Resultado do método" + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+
+}
